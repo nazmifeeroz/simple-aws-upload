460e8235d02ecbe3b226b4e27c1730ad2dc77edc
Nazmi <nazmifeeroz@hotmail.com>
2021-06-06 21:38:06 +0800

    vite config for build

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: "./runtimeConfig",
+        replacement: "./runtimeConfig.browser",
+      },
+    ],
+  },
+})
